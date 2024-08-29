e3137ca4248e68b5dbd2323ef61e5d9b070f365c
thinkofher <mail@bdudek.xyz>
2024-08-29 21:41:17 +0200

    add flags to cli version

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -24,12 +25,41 @@ func findFirstChild(n *html.Node) *html.Node {
 }
 
 func run() error {
+	var (
+		gomponentsAlias string
+		htmlAlias       string
+		htmlEnabled     bool
+		svgEnabled      bool
+	)
+
+	flag.StringVar(&gomponentsAlias, "gomponentsAlias", "g", "alias for gomponents package")
+	flag.StringVar(&htmlAlias, "htmlAlias", "", "alias for gomponents/html package")
+	flag.BoolVar(&htmlEnabled, "htmlEnabled", true, "render elements from gomponents/html package")
+	flag.BoolVar(&svgEnabled, "svg", false, "if true, koluszki will fully render svg elements")
+	flag.Parse()
+
 	n, err := html.Parse(os.Stdin)
 	if err != nil {
 		return fmt.Errorf("parse stream: %w", err)
 	}
 
-	if err := koluszki.Render(os.Stdout, findFirstChild(n)); err != nil {
+	opts := []koluszki.Option{}
+
+	if gomponentsAlias != "" {
+		opts = append(opts, koluszki.WithGomponentsAlias(gomponentsAlias))
+	}
+	if htmlEnabled {
+		opts = append(opts,
+			koluszki.WithHTMLPackageAttributes(gomponentsAlias, htmlAlias),
+			koluszki.WithHTMLPackageElements(gomponentsAlias, htmlAlias),
+		)
+	}
+	if svgEnabled {
+		opts = append(opts, koluszki.WithRenderSVG)
+	}
+
+	renderer := koluszki.NewRenderer(opts...)
+	if err := renderer.Render(os.Stdout, findFirstChild(n)); err != nil {
 		return fmt.Errorf("koluszki render: %w", err)
 	}
 
